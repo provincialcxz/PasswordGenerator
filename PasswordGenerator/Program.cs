53cf51086985754492f2aa708e5e722faa51df9e
provincialcxz <eremamihaylov@mail.ru>
2024-08-12 12:16:16 +0300

    feat: added project files

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+public class PasswordGenerator
+{
+    private static Random random = new Random();
+
+    public static string GeneratePassword(int minLength, int maxLength, string allowedChars,
+                                          int numberOfCharacters, string partialPassword = "",
+                                          string priorityChars = "", bool useUpper = true,
+                                          bool useLower = true, bool useDigits = true,
+                                          bool useSpecialChars = true, string specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?/`~")
+    {
+        int length = random.Next(minLength, maxLength + 1);
+        StringBuilder password = new StringBuilder(length);
+        StringBuilder availableChars = new StringBuilder(allowedChars);
+
+        if (useLower)
+            availableChars.Append("abcdefghijklmnopqrstuvwxyz");
+        if (useUpper)
+            availableChars.Append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        if (useDigits)
+            availableChars.Append("0123456789");
+        if (useSpecialChars && !string.IsNullOrEmpty(specialChars))
+            availableChars.Append(specialChars);
+
+        if (!string.IsNullOrEmpty(allowedChars))
+            availableChars = new StringBuilder(allowedChars);
+
+        if (!string.IsNullOrEmpty(priorityChars))
+            availableChars.Insert(0, priorityChars);
+
+        for (int i = 0; i < length; i++)
+        {
+            if (i < partialPassword.Length && partialPassword[i] != '_')
+            {
+                password.Append(partialPassword[i]);
+            }
+            else
+            {
+                password.Append(' ');
+            }
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            if (password[i] == ' ')
+            {
+                password[i] = availableChars[random.Next(availableChars.Length)];
+            }
+        }
+
+        char[] result = password.ToString().ToCharArray();
+        Array.Sort(result, (x, y) => random.Next(-1, 2));
+        return new string(result);
+    }
+
+    public static void GeneratePasswordsToFile(int numberOfPasswords, string filePath,
+                                               int minLength, int maxLength,
+                                               string allowedChars, int numberOfCharacters,
+                                               string partialPassword = "", string priorityChars = "",
+                                               bool useUpper = true, bool useLower = true,
+                                               bool useDigits = true, bool useSpecialChars = true,
+                                               string specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?/`~")
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            for (int i = 0; i < numberOfPasswords; i++)
+            {
+                string password = GeneratePassword(minLength, maxLength, allowedChars, numberOfCharacters, partialPassword, priorityChars, useUpper, useLower, useDigits, useSpecialChars, specialChars);
+
+                writer.WriteLine(password);
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int numberOfPasswords = 100;
+
+        string filePath = "passwords.txt";
+        int minLength = 8;
+        int maxLength = 12;
+
+
+        string allowedChars = "1q2wQwerty!;_";
+        string partialPassword = "1q2w____";
+        string priorityChars = "1q2wQwerty!;_";
+
+        bool useUpper = false;
+        bool useLower = false;
+        bool useDigits = false;
+        bool useSpecialChars = false;
+
+        string specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?/`~";
+
+        Console.WriteLine("Заданные параметры:");
+        Console.WriteLine("Кол-во паролей - " + numberOfPasswords);
+        Console.WriteLine("Длина паролей - от " + minLength + " до " + maxLength);
+        if (allowedChars != "") Console.WriteLine("Символы для генерации - " + allowedChars);
+        if (partialPassword != "") Console.WriteLine("Частичный ввод - " + partialPassword);
+        if (priorityChars != "") Console.WriteLine("Приоритетные символы - " + priorityChars);
+        Console.WriteLine("Заглавные символы - " + useUpper.ToString());
+        Console.WriteLine("Строчные символы - " + useLower.ToString());
+        Console.WriteLine("Цифры - " + useDigits.ToString());
+        Console.WriteLine("Спец. символы - " + useSpecialChars.ToString());
+        if(useSpecialChars) Console.WriteLine("Варианты спец. символов - " + specialChars);
+
+        Console.WriteLine("\n");
+
+        long memoryBefore = GC.GetTotalMemory(true);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        GeneratePasswordsToFile(numberOfPasswords, filePath, minLength, maxLength, allowedChars, numberOfPasswords, partialPassword, priorityChars, useUpper, useLower, useDigits, useSpecialChars, specialChars);
+        stopwatch.Stop();
+        TimeSpan ts = stopwatch.Elapsed;
+        long memoryAfter = GC.GetTotalMemory(true);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Время выполнения: {ts.TotalMilliseconds} мс");
+        Console.WriteLine($"Использование памяти: {memoryAfter - memoryBefore} байт");
+        Console.ResetColor();
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine($"Сгенерировано {numberOfPasswords} паролей и сохранено в файл {filePath}");
+        Console.WriteLine("Нажмите любую кнопку для продолжения...");
+        Console.ReadKey();
+    }
+}
---
+++ PasswordGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PasswordGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PasswordGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("54b0a3e3-47ce-4c97-b8d3-419f2e3705f5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
